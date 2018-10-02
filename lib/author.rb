b16b663ec52d546bfd4541813180d82617ff2537
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-02 03:53:54 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,26 +1,26 @@
 class Author
   attr_accessor :name, :posts
-  @@songs = []
+  @@posts = []
 
   def initialize(name)
     @name = name
     @posts = []
   end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@songs << song
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@posts << song
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     song.artist = self
-    @songs << song
+    @posts << song
     @@songs << song
   end
 
   def self.song_count
-    @@songs.length
+    @@posts.length
   end
 end
