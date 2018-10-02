df83fc205224a23c83c6c810f5b61c92765dfb2a
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-02 04:11:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,18 @@
 class Song
   attr_accessor :name, :artist
+  @@all = []
 
   def initialize(title)
     @name = title
+  end
+
+  def class_variable_get
+    @@all
+  end
+
+  def author_name
+    return nil if author.class != Author
+    author.name
   end
 
 end
